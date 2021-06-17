cf8e005aa56a9bc727cc2cd2271f3d9e3e8a127a
khangtm <tmkhang1712@gmail.com>
2021-06-17 19:25:06 +0700

    Add files via upload

---
+++ menu.js
@@ -0,0 +1,79 @@
+document.write(`
+<section class="elementor-section elementor-top-section elementor-element elementor-element-343c6450 revoworks-separation elementor-section-boxed elementor-section-height-default elementor-section-height-default background-color-layout"
+data-id="343c6450" data-element_type="section">
+   <div class="elementor-container elementor-column-gap-default">
+       <div class="elementor-row">
+           <div class="elementor-column elementor-col-100 elementor-top-column elementor-element elementor-element-7605b84d" data-id="7605b84d"
+            data-element_type="column">
+               <div class="elementor-column-wrap elementor-element-populated">
+                   <div class="elementor-widget-wrap">
+                       <div class="elementor-element elementor-element-59859754 elementor-widget elementor-widget-shortcode" data-id="59859754"
+                        data-element_type="widget" data-widget_type="shortcode.default">
+                           <div class="elementor-widget-container">
+                               <div class="elementor-shortcode">
+                                   <div data-elementor-type="page" data-elementor-id="9697" class="elementor elementor-9697" data-elementor-settings="[]">
+                                       <div class="elementor-inner">
+                                           <div class="elementor-section-wrap">
+                                               <section class="elementor-section elementor-top-section elementor-element elementor-element-4e399472 elementor-section-boxed elementor-section-height-default elementor-section-height-default"
+                                                data-id="4e399472" data-element_type="section">
+                                                   <div class="elementor-container elementor-column-gap-default">
+                                                       <div class="elementor-row">
+                                                           <div class="elementor-column elementor-col-100 elementor-top-column elementor-element elementor-element-5f6d6801" data-id="5f6d6801"
+                                                            data-element_type="column">
+                                                               <div class="elementor-column-wrap elementor-element-populated">
+                                                                   <div class="elementor-widget-wrap">
+                                                                       <div class="elementor-element elementor-element-47d477a7 elementor-widget elementor-widget-html" data-id="47d477a7" data-element_type="widget"
+                                                                        data-widget_type="html.default">
+                                                                           <div class="elementor-widget-container">
+                                                                               <div class="flex-container topmenu layout-grid">
+                                                                                   <div class="other-link menu-logo text-align-logo">
+                                                                                       <a href="index.html">
+                                                                                           <img src="./img/revo_mean_logo.png">
+                                                                                       </a>
+                                                                                   </div>
+
+                                                                                   <div class="other-link level1 layout-bodrer">
+                                                                                       <a href="about.html"><h6>RevoWorks Browserとは</h6></a>
+                                                                                       																									
+                                                                                   </div>
+
+                                                                                   <div class="other-link level2 layout-bodrer" style="width:70px">
+                                                                                       <a href="index.html" ><h6>料金</h6>	</a>
+                                                                                       																									
+                                                                                   </div>
+
+                                                                                   <div class="other-link level3 layout-bodrer">
+                                                                                       <a href="apply.html" ><h6>お申し込み</h6>	</a>
+                                                                                       																								
+                                                                                   </div>
+
+                                                                                   <div class="other-link level4 layout-bodrer">
+                                                                                       <a href="index.html" ><h6>サポート</h6></a>
+                                                                                       																										
+                                                                                   </div>
+                                                                                   <div class="other-link level5 layout-bodrer">
+                                                                                       <a href="login.html" ><h6>アカウント</h6>	</a>
+                                                                                       																									
+                                                                                   </div>
+                                                                               </div>
+                                                                           </div>
+                                                                       </div>
+                                                                   </div>
+                                                               </div>
+                                                           </div>
+                                                       </div>
+                                                   </div>
+                                               </section>
+                                           </div>
+                                       </div>
+                                   </div>
+                               </div>
+                           </div>
+                       </div>
+                   </div>
+               </div>
+           </div>
+       </div>
+   </div>
+</section>
+`);
