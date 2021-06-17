cb328b5073c5d13ecf73da15f295e0061a567ea4
khangtm <tmkhang1712@gmail.com>
2021-06-17 19:27:57 +0700

    Delete head.js

--- head.js
+++
@@ -1,9 +0,0 @@
-document.write(`
-<link rel="stylesheet" id="parent-style-css" href="./css/style.css" type="text/css" media="all">
-<link rel="stylesheet" id="common-style-css" href="./css/common.css" type="text/css" media="all">
-<link rel="stylesheet" id="design-style-css" href="./css/design.css" type="text/css" media="all">
-<link rel="stylesheet" id="theme-style-css" href="./css/theme.css" type="text/css" media="all">
-<link rel="stylesheet" id="elementor-frontend-css" href="./css/frontend.min.css" type="text/css" media="all">
-<link rel="stylesheet" id="elementor-global-css" href="./css/global.css" type="text/css" media="all">
-<link rel="stylesheet" id="font-awesome-css" href="./css/all.min.css" type="text/css" media="all"> 
-`);
