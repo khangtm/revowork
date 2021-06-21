6222c890891b304cf4ed50b436aa558ed52f4dc7
khangtm <tmkhang1712@gmail.com>
2021-06-21 17:12:14 +0700

    Update header.js

--- js/header.js
+++ js/header.js
@@ -17,16 +17,16 @@ document.write(`
         <div class="menu-%e3%83%98%e3%83%83%e3%83%80%e3%83%bc%e3%83%a1%e3%83%8b%e3%83%a5%e3%83%bc%ef%bc%88pc%e7%94%a8%ef%bc%89-container">
             <ul id="menu-%e3%83%98%e3%83%83%e3%83%80%e3%83%bc%e3%83%a1%e3%83%8b%e3%83%a5%e3%83%bc%ef%bc%88pc%e7%94%a8%ef%bc%89" class="menu">
                 <li id="menu-item-2174" style="padding-right: 20px;" class="faq menu-item menu-item-type-post_type menu-item-object-page menu-item-2174">
-                    <a href="index.html">よくあるご質問？</a>
+                    <a href="#">よくあるご質問？</a>
                 </li>
                 <li id="menu-item-2174" style="padding-right: 20px;" class="inquiry menu-item menu-item-type-post_type menu-item-object-page menu-item-2174">
-                    <a href="index.html">お問い合わせ</a>
+                    <a href="#">お問い合わせ</a>
                 </li>
                 <li id="menu-item-2174" style="padding-right: 20px;" class="mypagetop menu-item menu-item-type-post_type menu-item-object-page menu-item-2174">
                     <a href="mypage.html">マイページ</a>
                 </li>
                 <li id="menu-item-2174" class="building menu-item menu-item-type-post_type menu-item-object-page menu-item-2174">
-                    <a href="index.html" style="padding-right: 0px;">株式会社アクシス</a>
+                    <a href="#" style="padding-right: 0px;">株式会社アクシス</a>
                 </li>
             </ul>
         </div>
@@ -35,16 +35,16 @@ document.write(`
         <div class="menu-%e3%83%98%e3%83%83%e3%83%80%e3%83%bc%e3%83%a1%e3%83%8b%e3%83%a5%e3%83%bc%ef%bc%88pc%e7%94%a8%ef%bc%89-container">
             <ul id="menu-%e3%83%98%e3%83%83%e3%83%80%e3%83%bc%e3%83%a1%e3%83%8b%e3%83%a5%e3%83%bc%ef%bc%88pc%e7%94%a8%ef%bc%89" class="menu">
                 <li id="menu-item-2174" style="padding-right: 20px;" class="faq menu-item menu-item-type-post_type menu-item-object-page menu-item-2174">
-                    <a href="index.html">よくあるご質問？</a>
+                    <a href="#">よくあるご質問？</a>
                 </li>
                 <li id="menu-item-2174" style="padding-right: 20px;" class="inquiry menu-item menu-item-type-post_type menu-item-object-page menu-item-2174">
-                    <a href="index.html">お問い合わせ</a>
+                    <a href="#">お問い合わせ</a>
                 </li>
                 <li id="menu-item-2174" style="padding-right: 20px;" class="mypagetop menu-item menu-item-type-post_type menu-item-object-page menu-item-2174">
                     <a href="mypage.html">マイページ</a>
                 </li>
                 <li id="menu-item-2174" class="building menu-item menu-item-type-post_type menu-item-object-page menu-item-2174">
-                    <a href="index.html" style="padding-right: 0px;">株式会社アクシス</a>
+                    <a href="#" style="padding-right: 0px;">株式会社アクシス</a>
                 </li>
             </ul>
         </div>
@@ -66,9 +66,9 @@ document.write(`
         <div class="elementor-widget-container">
             <ul class="menu">
                 <li><a href="about.html" style="padding-right: 20px; padding-left: 20px; border: 1px solid #ddd;">RevoWorks Browserとは</a></li>
-                <li><a href="index.html" style="padding-right: 20px; padding-left: 20px;border: 1px solid #ddd;">料金</a></li>
+                <li><a href="#" style="padding-right: 20px; padding-left: 20px;border: 1px solid #ddd;">料金</a></li>
                 <li><a href="apply.html" style="padding-right: 20px; padding-left: 20px;border: 1px solid #ddd;">お申し込み</a></li>
-                <li><a href="index.html" style="padding-right: 20px; padding-left: 20px;border: 1px solid #ddd;">サポート</a></li>
+                <li><a href="#" style="padding-right: 20px; padding-left: 20px;border: 1px solid #ddd;">サポート</a></li>
                 <li><a href="login.html" style="padding-right: 20px; padding-left: 20px;border: 1px solid #ddd;">アカウント</a></li>
                 
             </ul>		
@@ -79,9 +79,9 @@ document.write(`
             <ul id="menu-%e3%83%98%e3%83%83%e3%83%80%e3%83%bc%e3%83%a1%e3%83%8b%e3%83%a5%e3%83%bc%ef%bc%88%e3%83%a2%e3%83%90%e3%82%a4%e3%83%ab%e7%94%a8%ef%bc%89"
             class="menu">
                 <li><a href="about.html" style="padding-right: 20px; padding-left: 20px; border: 1px solid #ddd;">RevoWorks Browserとは</a></li>
-                <li><a href="index.html" style="padding-right: 20px; padding-left: 20px;border: 1px solid #ddd;">料金</a></li>
+                <li><a href="#" style="padding-right: 20px; padding-left: 20px;border: 1px solid #ddd;">料金</a></li>
                 <li><a href="apply.html" style="padding-right: 20px; padding-left: 20px;border: 1px solid #ddd;">お申し込み</a></li>
-                <li><a href="index.html" style="padding-right: 20px; padding-left: 20px;border: 1px solid #ddd;">サポート</a></li>
+                <li><a href="#" style="padding-right: 20px; padding-left: 20px;border: 1px solid #ddd;">サポート</a></li>
                 <li><a href="login.html" style="padding-right: 20px; padding-left: 20px;border: 1px solid #ddd;">アカウント</a></li>
             </ul>
         </div>
